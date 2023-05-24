b0c5962ec706262c3bf3c4e7b13ba18039741150
maciek367 <maciek2baba2@gmail.com>
2023-05-24 18:45:41 +0200

    delete HelloWorld, start adding basic components, delete vite.svg

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import Layout from "./components/Layout.vue";
 </script>
 
 <template>
-  <HelloWorld />
+  <Layout />
 </template>
 
 <style scoped></style>
