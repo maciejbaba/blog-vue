c498e117e62404f186ea55fbfd2ca55dfb0c318b
maciek367 <maciek2baba2@gmail.com>
2023-05-24 18:36:31 +0200

    prettier, remove prop from HelloWorld

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,11 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <h1>Hii</h1>
 </template>
 
 <style scoped>
-  h1 {
-    color: red;
-  }
+h1 {
+  color: red;
+}
 </style>
