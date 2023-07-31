66d8dd4af4ffb0a467ebf99ba7508259781a9970
maciek367 <maciek2baba2@gmail.com>
2023-07-31 22:07:00 +0200

    add about me page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,13 +1,35 @@
 <template lang="">
-  <main>
-    
+  <main class="mx-auto w-1/2 mt-4 flex flex-col gap-4">
+    <c-heading>
+      <p>About me</p>
+      <hr />
+    </c-heading>
+    <p>
+      Lorem ipsum dolor sit amet consectetur
+      <span class="text-[#606c38] italic">adipisicing elit</span>.
+
+      <br /><br />
+      Quisquam voluptatibus, quibusdam, quia, quae voluptatem voluptatum quos
+      exercitationem
+      <span class="text-[#606c38] italic">quod voluptate quas</span> doloribus.
+      Quisquam voluptatibus, quibusdam, quia, quae voluptatem voluptatum quos
+      exercitationem quod voluptate quas doloribus.
+
+      <br /><br />
+      Quisquam voluptatibus, quibusdam, quia, quae
+      <span class="text-[#606c38] italic">voluptatem voluptatum</span> quos
+      exercitationem quod voluptate quas doloribus. Quisquam voluptatibus,
+      quibusdam, quia, quae voluptatem voluptatum quos exercitationem quod
+      voluptate quas doloribus.
+    </p>
   </main>
 </template>
 <script lang="ts">
+import { CHeading } from "@chakra-ui/vue-next";
 export default {
-  
-}
+  components: {
+    CHeading,
+  },
+};
 </script>
-<style lang="">
-  
-</style>
+<style lang=""></style>
