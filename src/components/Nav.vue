28eac8cadf21469fbd11a37779317c58c639706a
maciek367 <maciek2baba2@gmail.com>
2023-07-31 21:53:08 +0200

    change from normal links to clinks with router, style nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,11 +1,16 @@
-<template lang="">
-  <nav class="nav">
-    <a href="/">home</a>
-    <a href="/posts">posts</a>
-    <a href="/about">about</a>
-    <a href="/contact">contact</a>
+<template>
+  <nav class="flex gap-5 justify-between uppercase font-bold text-[#fefae0]">
+    <c-link as="router-link" to="/">home</c-link>
+    <c-link as="router-link" to="/posts">posts</c-link>
+    <c-link as="router-link" to="/about">about</c-link>
+    <c-link as="router-link" to="/contact">contact</c-link>
   </nav>
 </template>
 <script lang="ts">
-export default {};
+import { CLink } from "@chakra-ui/vue-next";
+export default {
+  components: {
+    CLink,
+  },
+};
 </script>
