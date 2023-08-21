83f179ffc142f3f786728de77a811c1f4e29b901
maciek367 <maciek2baba2@gmail.com>
2023-08-21 22:40:14 +0200

    add some classes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,12 @@
 <template>
-  <header>
-    <c-box class="flex justify-around items-center p-2 bg-[#bc6c25]">
-      <c-box class="flex gap-3 text-[#fefae0]">
-        <font-awesome-icon class="text-4xl" icon="fa-solid fa-blog" />
-        <c-heading class="font-bold italic">My Blog</c-heading>
-      </c-box>
-      <Nav />
+  <c-box class="flex justify-around items-center p-2 bg-[#bc6c25]">
+    <c-box class="flex gap-3 text-[#fefae0]">
+      <font-awesome-icon class="text-4xl" icon="fa-solid fa-blog" />
+      <c-heading class="font-bold italic">My Blog</c-heading>
     </c-box>
-    <c-box class="p-1 bg-[#dda15e]" />
-  </header>
+    <Nav />
+  </c-box>
+  <c-box class="p-1 bg-[#dda15e]" />
 </template>
 <script lang="ts">
 import Nav from "./Nav.vue";
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,37 @@
 <template>
-  <nav class="flex gap-5 justify-between uppercase font-bold text-[#fefae0]">
-    <c-link as="router-link" to="/">home</c-link>
-    <c-link as="router-link" to="/posts">posts</c-link>
-    <c-link as="router-link" to="/about">about</c-link>
-    <c-link as="router-link" to="/contact">contact</c-link>
-  </nav>
+  <c-box class="flex gap-5 justify-between uppercase font-bold text-[#fefae0]">
+    <c-link
+      class="hover:text-[#dda15e] relative inline-block transition duration-500 ease-in-out before:content-[''] before:absolute before:bottom-[-5px] before:left-0 before:w-full before:h-[5px] before:bg-[#dda15e] before:opacity-0 before:transition-all before:duration-500 before:ease-in-out hover:before:opacity-100"
+      as="router-link"
+      to="/"
+      >home</c-link
+    >
+    <c-link
+      class="hover:text-[#dda15e] transition duration-500 ease-in-out"
+      as="router-link"
+      to="/posts"
+      >posts</c-link
+    >
+    <c-link
+      class="hover:text-[#dda15e] transition duration-500 ease-in-out"
+      as="router-link"
+      to="/about"
+      >about</c-link
+    >
+    <c-link
+      class="hover:text-[#dda15e] transition duration-500 ease-in-out"
+      as="router-link"
+      to="/contact"
+      >contact</c-link
+    >
+  </c-box>
 </template>
 <script lang="ts">
-import { CLink } from "@chakra-ui/vue-next";
+import { CLink, CBox } from "@chakra-ui/vue-next";
 export default {
   components: {
     CLink,
+    CBox,
   },
 };
 </script>
