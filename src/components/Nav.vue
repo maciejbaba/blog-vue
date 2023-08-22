c28d5e449c9990636c1ce0c54b6208a1666a1ea2
maciek367 <maciek2baba2@gmail.com>
2023-08-22 13:45:30 +0200

    add nav styles

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,27 +1,36 @@
+<!--
+  style="text-decoration: none;"
+  is used to remove the underline from the link
+-->
+
 <template>
   <c-box class="flex gap-5 justify-between uppercase font-bold text-[#fefae0]">
     <c-link
-      class="hover:text-[#dda15e] relative inline-block transition duration-500 ease-in-out before:content-[''] before:absolute before:bottom-[-5px] before:left-0 before:w-full before:h-[5px] before:bg-[#dda15e] before:opacity-0 before:transition-all before:duration-500 before:ease-in-out hover:before:opacity-100"
+      class="hover:text-[#dda15e] relative inline-block transition duration-500 ease-in-out before:content-[''] before:absolute before:bottom-[-5px] before:left-0 before:w-0 hover:before:w-full before:h-[5px] before:bg-[#dda15e] before:opacity-100 before:transition-all before:duration-500 before:ease-in-out"
       as="router-link"
+      style="text-decoration: none"
       to="/"
       >home</c-link
     >
     <c-link
-      class="hover:text-[#dda15e] transition duration-500 ease-in-out"
+      class="hover:text-[#dda15e] relative inline-block transition duration-500 ease-in-out before:content-[''] before:absolute before:bottom-[-5px] before:left-0 before:w-0 hover:before:w-full before:h-[5px] before:bg-[#dda15e] before:opacity-100 before:transition-all before:duration-500 before:ease-in-out"
       as="router-link"
       to="/posts"
+      style="text-decoration: none"
       >posts</c-link
     >
     <c-link
-      class="hover:text-[#dda15e] transition duration-500 ease-in-out"
+      class="hover:text-[#dda15e] relative inline-block transition duration-500 ease-in-out before:content-[''] before:absolute before:bottom-[-5px] before:left-0 before:w-0 hover:before:w-full before:h-[5px] before:bg-[#dda15e] before:opacity-100 before:transition-all before:duration-500 before:ease-in-out"
       as="router-link"
       to="/about"
+      style="text-decoration: none"
       >about</c-link
     >
     <c-link
-      class="hover:text-[#dda15e] transition duration-500 ease-in-out"
+      class="hover:text-[#dda15e] relative inline-block transition duration-500 ease-in-out before:content-[''] before:absolute before:bottom-[-5px] before:left-0 before:w-0 hover:before:w-full before:h-[5px] before:bg-[#dda15e] before:opacity-100 before:transition-all before:duration-500 before:ease-in-out"
       as="router-link"
       to="/contact"
+      style="text-decoration: none"
       >contact</c-link
     >
   </c-box>
