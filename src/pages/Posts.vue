0580701d4e53451d929d420317e571ca702ac52a
maciejbaba <maciek2baba2@gmail.com>
2024-03-04 16:23:05 +0100

    add fetching posts from db

--- src/pages/Posts.vue
+++ src/pages/Posts.vue
@@ -1,13 +1,28 @@
 <template lang="">
   <main>
-    
+    <div v-for="post in posts" :key="post.id">
+      <h2>{{ post.id }} - {{ post.title }}</h2>
+      <p>{{ post.content }}</p>
+    </div>
   </main>
 </template>
 <script lang="ts">
 export default {
-  
-}
+  mounted() {
+    this.getPosts();
+  },
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  methods: {
+    getPosts: function () {
+      fetch("http://localhost:3000/posts")
+        .then((response) => response.json())
+        .then((data) => (this.posts = data));
+    },
+  },
+};
 </script>
-<style lang="">
-  
-</style>
+<style lang=""></style>
